eceb5e45ecf01e9f096e5bd977517f08cb33d282
minspan1999 <minspan@users.noreply.github.com>
2020-03-26 13:00:13 -0400

    Update bandstructuressh.m

--- bandstructuressh.m
+++ bandstructuressh.m
@@ -1,12 +1,12 @@
 clear all;
-
+close all;
 ksA = 0;
 t1 = 0.5;t2 = 1;
 k = 1;
 
 
 r = 0;
-for kb = -pi:0.05:pi
+for kb = -pi:0.01:pi
     H = [1i*r t1 + t2*exp(-1i*kb);t1 + t2*exp(1i*kb) -1i*r];
     tem{k} = eig(H);
     ksp(k) = tem{k}(1);
@@ -15,13 +15,16 @@ for kb = -pi:0.05:pi
 end
 
 figure
-plot(-pi:0.05:pi,real(ksp),'b.');
+plot(-pi:0.01:pi,real(ksp),'b.');
 hold on;
-plot(-pi:0.05:pi,real(ksm),'b.');
+plot(-pi:0.01:pi,real(ksm),'b.');
 set(gca,'xtick',[-pi,-pi/2,0,pi/2,pi]);
 set(gca,'xticklabel',{'-\pi','-\pi/2','0','\pi/2','\pi'});
 set(gca,'Fontname','Times New Roman')
 set(gcf, 'Position', [00, 00, 350, 300])
 set(gca,'Fontsize',10)
 % legend('Re[\epsilon]','Im[\epsilon]')
-xlim([-pi pi]);ylim([-2 2])
+% xlabel('k');
+set(gca,'FontSize', 14) % Font Size
+xlim([-pi pi]);
+ylim([-2.2 2.2])
